0a27dd642773146cbeefc2fee9d00c9f1de16c7a
Sadegh Mohebbi <sadegh.mohebbi@inside.sahab.ir>
2025-05-19 10:33:36 +0330

    add gitignore

--- kubernetes-training/create-secret.sh
+++ kubernetes-training/create-secret.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -x
+openssl req -x509 -newkey rsa:4096 -keyout kuard.key -out kuard.crt -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=CommonNameOrHostname"
 minikube kubectl -- create secret generic kuard-tls --from-file=kuard.crt --from-file=kuard.key
